544d6ca6f49a8527127ab8630aa2c947ea70207e
Mario Clavijo <clavijomarioa@outlook.com>
2023-03-12 21:48:15 -0500

    Finished seeding db

--- src/ormseedconfig.ts
+++ src/ormseedconfig.ts
@@ -1,7 +1,6 @@
 import ormconfig from '@app/ormconfig'
 const ormseedconfig = {
     ...ormconfig,
-    // migrations: [__dirname + 'src/seeds/*.ts'],
     migrations: [ __dirname + '/seeds/*.ts'],
 };
 export default ormseedconfig;
