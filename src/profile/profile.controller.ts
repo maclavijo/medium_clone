3b4d891d606c6c022f2788142894c4f833b6346c
Mario Clavijo <clavijomarioa@outlook.com>
2023-03-13 23:05:36 -0500

    Adding profile module

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,13 +1,21 @@
 import { User } from '@app/user/decorators/user.decorator';
 import { AuthGuard } from '@app/user/guards/auth.guard';
-import { Controller, Get, Param, UseGuards, UsePipes } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileResponseInterface } from './types/profileResponseInterface';
 import { UserEntity } from '@app/user/user.entity';
 
 @Controller('profiles')
 export class ProfileController {
-    constructor(private readonly profileService: ProfileService) {}
+  constructor(private readonly profileService: ProfileService) {}
 
   @Get(':username')
   @UseGuards(AuthGuard)
@@ -21,4 +29,31 @@ export class ProfileController {
     );
     return this.profileService.buildProfileResponse(profile);
   }
+
+  @Post(':username/follow')
+  @UseGuards(AuthGuard)
+  async followProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.followProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile)
+  }
+
+  @Delete(':username/follow')
+  @UseGuards(AuthGuard)
+  async unfollowProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.unfollowProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile)
+  }
+
 }
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,36 +1,123 @@
-import { UserEntity } from "@app/user/user.entity";
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { ProfileType } from "./types/profile.type";
-import { ProfileResponseInterface } from "./types/profileResponseInterface";
+import { UserEntity } from '@app/user/user.entity';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { FollowEntity } from './follow.entity';
+import { ProfileType } from './types/profile.type';
+import { ProfileResponseInterface } from './types/profileResponseInterface';
 
 @Injectable()
 export class ProfileService {
-    constructor(
-        @InjectRepository(UserEntity)
-        private readonly userRepository: Repository<UserEntity>,
-    ) {}
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
+  ) {}
 
-    getProfile(
-        currentUserId: number,
-        profileUsername: string,
-    ): Promise<ProfileType>{
-        const user = await this.userRepository.findOne({
-            where: {
-                username: profileUsername,
-            },
-        });
+  async getProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: {
+        username: profileUsername,
+      },
+    });
 
-        if (!user) {
-            throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
-        }
-
-        return { ...user , following: false };
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
     }
 
-    buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
-        return { profile };
-        };
-      }
+    return { ...user, following: false };
+  }
+
+  async followProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: {
+        username: profileUsername,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'Follower and following cant be the same',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const follow = await this.followRepository.findOne({
+      where: {
+        followerId: currentUserId,
+        followingId: user.id,
+      },
+    });
+
+    if (!follow) {
+        const followToCreate = new FollowEntity()
+        followToCreate.followerId = currentUserId;
+        followToCreate.followingId = user.id;
+        await this.followRepository.save(followToCreate);
+    }
+
+    return { ...user, following: true };
+  }
+
+
+  async unfollowProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: {
+        username: profileUsername,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'Follower and following cant be the same',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const unfollow = await this.followRepository.findOne({
+      where: {
+        followerId: currentUserId,
+        followingId: user.id,
+      },
+    });
+
+    if (unfollow) {
+        throw new HttpException(
+            'You are not following this user',
+            HttpStatus.BAD_REQUEST,
+            );
+    }
+
+    // if (!unfollow) {
+    //     const followToCreate = new FollowEntity()
+    //     followToCreate.followerId = currentUserId;
+    //     followToCreate.followingId = user.id;
+    //     await this.followRepository.save(followToCreate);
+    // }
+
+    return { ...user, following: false };
+  }
+
+  buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
+    delete profile.email;
+    return { profile };
+  }
 }
